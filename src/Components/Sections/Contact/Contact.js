13565750cbcb9bfdd96b10d11008d40b146d56c5
Brian De Maio <briandemaio@Brians-MacBook-Air.local>
2021-11-25 15:00:38 -0500

    Updating styling on sections for grid table

--- src/Components/Sections/Contact/Contact.js
+++ src/Components/Sections/Contact/Contact.js
@@ -10,30 +10,35 @@ const Contact = ({ styles }) => {
 
       <h2 className="lime-text text-darken-2">Contact Me</h2>
 
-      <div className={contactStyles['contact-information']}>
+      <div className={contactStyles["contact-information"]}>
         <div className="row">
           <div className="col s6 l3">
             <div className="icon">
               <i className="material-icons">email</i>
             </div>
-            <p>bdemaio1@gmail.com</p>
+            <a href="mailto:bdemaio1@gmail.com">
+              <p>bdemaio1@gmail.com</p>
+            </a>
           </div>
 
           <div className="col s6 l3">
             <div className="icon">
               <i className="material-icons">phone_iphone</i>
             </div>
-            <p>(407)797-9158</p>
+            <a href="tel:+4077979158">
+              <p>(407)797-9158</p>
+            </a>
           </div>
-
           <div className="col s6 l3">
             <div className="icon">
               <i className="material-icons">location_on</i>
             </div>
-            <p>
-              Orlando <br />
-              FL, USA
-            </p>
+            <a href="http://maps.google.com/?q=Orlando">
+              <p>
+                Orlando <br />
+                FL, USA
+              </p>
+            </a>
           </div>
           <div className="clearfix"></div>
         </div>
