6d85d8a95c4d93c662b74750d7daa4b6eac8756a
Brian De Maio <briandemaio@Brians-MacBook-Air.local>
2021-12-01 21:50:58 -0500

    Adding portfolio items as links within swipeable swimlane

--- src/Components/Sections/Portfolio/Portfolio.js
+++ src/Components/Sections/Portfolio/Portfolio.js
@@ -2,6 +2,7 @@ import React from "react";
 import { Navigation } from "swiper";
 import { Swiper, SwiperSlide } from "swiper/react/swiper-react";
 import "swiper/swiper-bundle.min.css";
+import portfolioStyles from "./Portfolio.module.css";
 import InspaceImage from "../../../assets/images/Portfolio/portfolio_inspace.png";
 import ReactRollImage from "../../../assets/images/Portfolio/react_roll.png";
 import TheOperationImage from "../../../assets/images/Portfolio/the_operation.png";
@@ -10,38 +11,38 @@ const Portfolio = ({ styles }) => {
   return (
     <section id="portfolio" className={styles.section}>
       <span
-        className={`${styles.icon} btn-floating btn-large deep-purple darken-3`}
+        className={`${styles.icon} btn-floating btn-large light-bg darken-3`}
       >
         <i className="material-icons">important_devices</i>
       </span>
-      <h2 className="deep-purple-text text-darken-3">Services</h2>
+      <h2 className="deep-purple-text text-darken-3">Portfolio</h2>
       <div id="Services_Carousel">
         <Swiper
           modules={[Navigation]}
-          spaceBetween={50}
+          spaceBetween={200}
           slidesPerView={1}
           navigation
           breakpoints={{
             768: {
               width: 768,
-              slidesPerView: 3,
+              slidesPerView: 2,
             },
           }}
         >
           <SwiperSlide>
-            <div>
+            <a href="https://www.theoperation.org" className={portfolioStyles['portfolio-item-container']}>
               <img src={TheOperationImage}/>
-            </div>
+            </a>
           </SwiperSlide>
           <SwiperSlide>
-            <div>
+            <a href="https://www.inspace.info" className={portfolioStyles['portfolio-item-container']}>
                 <img src={InspaceImage}/>
-            </div>
+            </a>
           </SwiperSlide>
           <SwiperSlide>
-            <div>
+            <a href="https://www.reactroll.com" className={portfolioStyles['portfolio-item-container']}>
                 <img src={ReactRollImage}/>
-            </div>
+            </a>
           </SwiperSlide>
         </Swiper>
       </div>
